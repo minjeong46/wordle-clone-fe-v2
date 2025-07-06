d01b336421a8bfb5381fbc7d71962932006a745f
minjeong46 <eyou46@naver.com>
2025-07-06 18:04:03 +0900

    js  handleKeyInput 수정
    서버에서 대문자로 정답을 지정해주면 값이 입력되지 않아 대문자를 기준으로  입력 함수 수정

--- static/app.js
+++ static/app.js
@@ -1,6 +1,5 @@
 let count = 0;
 let rowCount = 0;
-const 정답 = "magic";
 let timer;
 
 function appStart() {
@@ -24,37 +23,42 @@ function appStart() {
             `.block-box__item[data-item='${rowCount}${count}']`
         );
 
-        if (key === "Backspace") handelKeyBackspace();
-        if (count === 5) {
-            if (key === "Enter") handleKeyEnterInput();
+        if (key === "BACKSPACE") handelKeyBackspace();
+        else if (count === 5) {
+            if (key === "ENTER") handleKeyEnterInput();
             else return;
-        } else if (key >= "a" && key <= "z") {
+        } else if (key >= "A" && key <= "Z") {
             block.innerHTML = key;
             count++;
-        }
-        
-    }
+        } 
+    };
 
-    const handleKeyEnterInput = () => {
+    const handleKeyEnterInput = async () => {
         let 맞은_수 = 0;
+        const 응답 = await fetch("/answer"); //해당 루트에 응답을 받고
+        console.log(응답);
+        const 정답_객체 = await 응답.json(); // 그 응답은 json 으로 포맷
+        console.log(정답_객체);
+        const 정답 = 정답_객체.answer;
+
         for (let i = 0; i < 5; i++) {
             const block = document.querySelector(
                 `.block-box__item[data-item='${rowCount}${i}']`
             );
 
-            const 입력_정답 = block.innerHTML;
+            const 입력_정답 = block.innerHTML.toLocaleUpperCase();
             const 정답_글자 = 정답[i];
 
             if (입력_정답 === 정답_글자) {
+                block.style =
+                    "background-color:#B49F3A; color: white; transition: all 0.3s ease-in-out;";
 
-                block.style = "background-color:#B49F3A; color: white; transition: all 0.3s ease-in-out;"
-
-                document.querySelectorAll(`.key-box__item`).forEach((e)=>{
-                    if(e.dataset.key === 정답_글자){
+                document.querySelectorAll(`.key-box__item`).forEach((e) => {
+                    if (e.dataset.key === 정답_글자) {
                         e.style.backgroundColor = "#B49F3A";
                         block.style.color = "white";
                     }
-                })
+                });
                 block.classList.add("bounce");
 
                 맞은_수++;
@@ -62,22 +66,22 @@ function appStart() {
                 block.style.backgroundColor = "#538D4E";
                 block.style.color = "white";
 
-                document.querySelectorAll(`.key-box__item`).forEach((e)=>{
-                    if(e.dataset.key === 입력_정답){
+                document.querySelectorAll(`.key-box__item`).forEach((e) => {
+                    if (e.dataset.key === 입력_정답) {
                         e.style.backgroundColor = "#538D4E";
                         block.style.color = "white";
                     }
-                })
+                });
             } else {
                 block.style.backgroundColor = "#3A3A3C";
                 block.style.color = "white";
 
-                document.querySelectorAll(`.key-box__item`).forEach((e)=>{
-                    if(e.dataset.key === 입력_정답){
+                document.querySelectorAll(`.key-box__item`).forEach((e) => {
+                    if (e.dataset.key === 입력_정답) {
                         e.style.backgroundColor = "#efefef";
                         block.style.color = "white";
                     }
-                })
+                });
 
                 block.classList.add("shake");
             }
@@ -91,10 +95,11 @@ function appStart() {
             const keyItem = document.querySelector(
                 `.block-box__item[data-item='${rowCount}${count - 1}']`
             );
+            console.log(keyItem)
             keyItem.innerHTML = "";
         }
 
-        if (count !== 0) count--;
+        if (count !== 0) count -= 1;
     };
 
     const gameOver = () => {
@@ -119,7 +124,7 @@ function appStart() {
 
     startTimer();
     window.addEventListener("keydown", (e) => {
-        const key = e.key;
+        const key = e.key.toUpperCase();
         handleKeyInput(key);
     });
     window.addEventListener("click", (e) => {
